eee5460e0da31153b5d7895ce82fd84b85f38a46
dudu <67109662@qq.com>
2019-04-18 09:42:43 +0800

    添加注册和404页面

---
+++ src/views/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="notFound">
+    <img src="../assets/404.svg" alt>
+  </div>
+</template>
+
+<style scoped>
+.notFound {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.notFound img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  margin: auto;
+}
+</style>
